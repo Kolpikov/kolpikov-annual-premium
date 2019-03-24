6f2d8046361cb2c814e23d86940b009734cd95e5
Andrew Kolpikov <kolpikov.andrey@gmail.com>
2019-03-25 00:29:11 +0200

    * Query for charts;

--- src/Models/AnnualPremium.php
+++ src/Models/AnnualPremium.php
@@ -2,8 +2,10 @@
 
 namespace Kolpikov\AnnualPremium\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\DB;
 
 /**
  * Class AnnualPremium
@@ -43,5 +45,20 @@ class AnnualPremium extends Model
     protected function gender(): BelongsTo
     {
         return $this->belongsTo(Gender::class);
+    }
+
+    /**
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeRates(Builder $query): Builder
+    {
+        return $query->select([
+            'annual_premiums.age',
+            'annual_premiums.gender_id',
+            DB::raw('ROUND(AVG(min_ann_prem),2) as min_ann_prem'),
+            DB::raw('ROUND(AVG(max_ann_prem),2) as max_ann_prem'),
+            DB::raw('ROUND(AVG((min_ann_prem + max_ann_prem) / 2),2) as avg_ann_prem'),
+        ])->groupBy('age', 'gender_id')->orderBy('gender_id');
     }
 }
